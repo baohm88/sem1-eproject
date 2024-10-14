efcbf136a790ea97c80c3fc91ba074a69564f309
Bao Ha <baohm88@gmail.com>
2024-10-14 22:19:51 +0700

    synced with team2fly

--- backend/app/controllers/CollectionsController.php
+++ backend/app/controllers/CollectionsController.php
@@ -10,7 +10,7 @@ class CollectionsController extends BaseController
     public function all()
     {
         $products = $this->__instanceModel->getAllProducts();
-        $this->FactoryMessage("info", "This is products obj", $products);
+        $this->FactoryMessage("success", "This is products obj", $products);
     }
 
     public function skincare($params = [])
@@ -67,6 +67,8 @@ class CollectionsController extends BaseController
 
         // If the product exists, return it; otherwise, return an error message
         if ($product) {
+            $product_ratings = $this->__instanceModel->getProductRatingsById($productId);
+            $product['product_ratings'] = $product_ratings; // Corrected syntax here
             $this->FactoryMessage("success", "Product found", $product);
         } else {
             $this->FactoryMessage("error", "Product not found");
