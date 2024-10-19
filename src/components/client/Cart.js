44196afebf3897a7f1ed84598f241d6e2a4a726c
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2024-10-19 10:51:57 +0700

    adde styling to cart

--- src/components/client/Cart.js
+++ src/components/client/Cart.js
@@ -78,119 +78,139 @@ export default function Cart() {
                 <span className="error-message">({serverError})</span>
             )}
 
-            <div className={classes.cartContainer}>
-                <h1>Your products</h1>
-                <table>
-                    <tbody>
-                        {cart.map((item) => {
-                            return (
-                                <tr key={item.product_id}>
-                                    <td>
-                                        <Link
-                                            to={"/products/" + item.product_id}
-                                        >
-                                            <img
-                                                src={
-                                                    item.product_images
-                                                        ? item.product_images.split(
-                                                              ","
-                                                          )[0]
-                                                        : ""
-                                                }
-                                                alt={item.product_name}
-                                                style={{ width: "5rem" }}
-                                            />
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <Link
-                                            to={"/products/" + item.product_id}
-                                            className={classes.productName}
-                                        >
-                                            {item.product_name}
-                                        </Link>
-                                    </td>
-                                    <td className={classes.center}>
-                                        <div
-                                            className={classes.quantityControl}
-                                        >
-                                            <span
-                                                className={classes.adjustButton}
-                                                onClick={() =>
-                                                    decrementQuantity(
-                                                        item.product_id
-                                                    )
+            {/* Wrapper for the cart layout */}
+            <div className={classes.cartLayout}>
+                <div className={classes.cartContainer}>
+                    <h1>Your products</h1>
+                    <table>
+                        <tbody>
+                            {cart.map((item) => {
+                                return (
+                                    <tr key={item.product_id}>
+                                        <td>
+                                            <Link
+                                                to={
+                                                    "/products/" +
+                                                    item.product_id
                                                 }
                                             >
-                                                -
-                                            </span>
-                                            <span className={classes.quantity}>
-                                                {item.quantity}
-                                            </span>
-                                            <span
-                                                className={classes.adjustButton}
-                                                onClick={() =>
-                                                    incrementQuantity(
-                                                        item.product_id
-                                                    )
+                                                <img
+                                                    src={
+                                                        item.product_images
+                                                            ? item.product_images.split(
+                                                                  ","
+                                                              )[0]
+                                                            : ""
+                                                    }
+                                                    alt={item.product_name}
+                                                    style={{ width: "5rem" }}
+                                                />
+                                            </Link>
+                                        </td>
+                                        <td>
+                                            <Link
+                                                to={
+                                                    "/products/" +
+                                                    item.product_id
+                                                }
+                                                className={classes.productName}
+                                            >
+                                                {item.product_name}
+                                            </Link>
+                                        </td>
+                                        <td className={classes.center}>
+                                            <div
+                                                className={
+                                                    classes.quantityControl
                                                 }
                                             >
-                                                +
+                                                <span
+                                                    className={
+                                                        classes.adjustButton
+                                                    }
+                                                    onClick={() =>
+                                                        decrementQuantity(
+                                                            item.product_id
+                                                        )
+                                                    }
+                                                >
+                                                    -
+                                                </span>
+                                                <span
+                                                    className={classes.quantity}
+                                                >
+                                                    {item.quantity}
+                                                </span>
+                                                <span
+                                                    className={
+                                                        classes.adjustButton
+                                                    }
+                                                    onClick={() =>
+                                                        incrementQuantity(
+                                                            item.product_id
+                                                        )
+                                                    }
+                                                >
+                                                    +
+                                                </span>
+                                            </div>
+                                        </td>
+                                        <td className={classes.center}>
+                                            {formatter.format(
+                                                item.product_price
+                                            )}
+                                        </td>
+                                        <td className={classes.center}>
+                                            {formatter.format(
+                                                item.product_price *
+                                                    item.quantity
+                                            )}
+                                        </td>
+                                        <td className={classes.center}>
+                                            <span
+                                                className={classes.removeButton}
+                                                onClick={() =>
+                                                    removeItem(item.product_id)
+                                                }
+                                            >
+                                                X
                                             </span>
-                                        </div>
-                                    </td>
-                                    <td className={classes.center}>
-                                        {formatter.format(item.product_price)}
-                                    </td>
-                                    <td className={classes.center}>
-                                        {formatter.format(
-                                            item.product_price * item.quantity
-                                        )}
-                                    </td>
-                                    <td className={classes.center}>
-                                        <span
-                                            className={classes.removeButton}
-                                            onClick={() =>
-                                                removeItem(item.product_id)
-                                            }
-                                        >
-                                            X
-                                        </span>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
 
-            <div className={classes.orderSummary}>
-                <h3>Order Summary</h3>
-                <hr />
-                <p className={classes.flexContainerBetween}>
-                    <span>Subtotal</span>{" "}
-                    <span>{formatter.format(totalAmount)}</span>
-                </p>
-                <p className={classes.flexContainerBetween}>
-                    <span>Shipping</span> <span>$0</span>
-                </p>
-                <hr />
-                <p>Tax will be calculated during checkout</p>
-                <p className={classes.flexContainerBetween}>
-                    <span>
-                        <strong>Estimated Total</strong>
-                    </span>{" "}
-                    <span>
-                        <strong>{formatter.format(totalAmount)}</strong>
-                    </span>
-                </p>
-                <Button
-                    className="full-width-button"
-                    onClick={handleCheckout}
-                    disabled={cart.length === 0}
-                >
-                    Checkout
-                </Button>
+                <div className={classes.orderSummary}>
+                    <h3>Order Summary</h3>
+                    <hr />
+                    <p className={classes.flexContainerBetween}>
+                        <span>Subtotal</span>{" "}
+                        <span>{formatter.format(totalAmount)}</span>
+                    </p>
+                    <p className={classes.flexContainerBetween}>
+                        <span>Shipping</span> <span>$0</span>
+                    </p>
+                    <hr />
+                    <p>Tax will be calculated during checkout</p>
+                    <p className={classes.flexContainerBetween}>
+                        <span>
+                            <strong>Estimated Total</strong>
+                        </span>{" "}
+                        <span>
+                            <strong>{formatter.format(totalAmount)}</strong>
+                        </span>
+                    </p>
+                    <Button
+                        className="full-width-button"
+                        onClick={handleCheckout}
+                        disabled={cart.length === 0}
+                    >
+                        Checkout
+                    </Button>
+                </div>
             </div>
         </div>
     );
