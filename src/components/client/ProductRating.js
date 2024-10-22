a9fbbee6cc50a90cbcb72e85db86da7a256bb43e
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2024-10-22 16:11:30 +0700

    fix bug in handleSubmitReview

--- src/components/client/ProductRating.js
+++ src/components/client/ProductRating.js
@@ -16,7 +16,8 @@ export default function ProductRating({ rating }) {
             <div>
                 <p>{renderReviewStars(rating.rating)} </p>
                 <h5>
-                    By: {rating.username} on {rating.review_date}
+                    By: {rating.username} on{" "}
+                    {new Date(rating.review_date).toLocaleDateString()}
                 </h5>
                 <p className="rating-comment">
                     <i>{rating.rating_comment}</i>
